4e5243154522ed63377bec850792ed5529ae8357
Jordan Samson <jsamson2@rheinmetall.ca>
2019-02-14 11:20:37 -0500

    branch commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        helloWorld();
+    }
+
+    private static void helloWorld()
+    {
+        System.out.println("Hello world");
     }
 }
